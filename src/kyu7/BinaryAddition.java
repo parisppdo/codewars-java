cd21e716e8189765727a646033b2460c30e2acce
Paris Papadopoulos <parisppdo@protonmail.com>
2025-11-19 12:09:20 +0200

    created Binary Addition

---
+++ src/kyu7/BinaryAddition.java
@@ -0,0 +1,12 @@
+package kyu7;
+
+// Kata: Binary Addition
+// http://codewars.com/kata/551f37452ff852b7bd000139
+
+public class BinaryAddition {
+    public static String binaryAddition(int a, int b){
+        //your code here
+        return "";
+    }
+}
+gi
